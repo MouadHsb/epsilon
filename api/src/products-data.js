e7358ee8520677cf446bb0f1197773efd32afc1a
MouadHsb <152964260+MouadHsb@users.noreply.github.com>
2025-05-08 10:20:59 +0100

    serverless function error test revert

--- api/src/products-data.js
+++
@@ -1,4 +0,0 @@
-// /api/products-data.js
-// Copy the content from api/src/models/product.js here
-// Or export it directly from your original file
-export { default } from '../api/src/models/product.js';
