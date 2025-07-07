d5242b54c044f8da7b0ac7ed075d76beafb2192b
MouadHsb <152964260+MouadHsb@users.noreply.github.com>
2025-07-07 18:42:46 +0100

    reductions complete

--- src/data/products.js
+++ src/data/products.js
@@ -4,7 +4,7 @@ const products = [
     id: 11,
     name: "Thuya & Epoxy Coffee Table",
     description: "This exceptional coffee table combines the natural beauty of thuya wood with the modern appeal of epoxy resin. Each piece features unique grain patterns and a flowing epoxy that creates a mesmerizing focal point. Handcrafted with precision and finished with multiple coats of protective polyurethane for durability.",
-    originalPrice: 2500.00,
+    originalPrice: 2650.00,
     price: 2000.00,
     category: "Coffee Tables",
     size: "60cm x 40cm x 45cm",
@@ -19,8 +19,8 @@ const products = [
     id: 12,
     name: "Walnut & Epoxy Coffee Table",
     description: "This exceptional coffee table combines the natural beauty of walnut wood with the modern appeal of epoxy resin. Each piece features unique grain patterns and a flowing epoxy that creates a mesmerizing focal point. Handcrafted with precision and finished with multiple coats of protective polyurethane for durability.",
-    originalPrice: 2400.00,
-    price: 2000.00,
+    originalPrice: 3500.00,
+    price: 2500.00,
     category: "Coffee Tables",
     size: "55cm x 55cm x 45cm",
     featured: false,
@@ -34,8 +34,8 @@ const products = [
     id: 13,
     name: "Walnut & Epoxy Coffee Table",
     description: "This exceptional coffee table combines the natural beauty of walnut wood with the modern appeal of epoxy resin. Each piece features unique grain patterns and a flowing epoxy that creates a mesmerizing focal point. Handcrafted with precision and finished with multiple coats of protective polyurethane for durability.",
-    originalPrice: 2300.00,
-    price: 2000.00,
+    originalPrice: 3500.00,
+    price: 2500.00,
     category: "Coffee Tables",
     size: "55cm x 55cm x 45cm",
     featured: false,
@@ -48,8 +48,8 @@ const products = [
     id: 14,
     name: "Walnut & Epoxy Coffee Table",
     description: "This exceptional coffee table combines the natural beauty of walnut wood with the modern appeal of epoxy resin. Each piece features unique grain patterns and a flowing epoxy that creates a mesmerizing focal point. Handcrafted with precision and finished with multiple coats of protective polyurethane for durability.",
-    originalPrice: 2200.00,
-    price: 2000.00,
+    originalPrice: 3500.00,
+    price: 2500.00,
     category: "Coffee Tables",
     size: "55cm x 55cm x 45cm",
     featured: false,
@@ -96,6 +96,45 @@ const products = [
     featured: true,
     images: [
       "/pd231.jpeg",
+    ]
+  },
+  {
+    "id": 31,
+    "name": "Thuya & Epoxy River Desk",
+    "description": "Elevate your workspace with this stunning Thuya Burl & Epoxy River Desk. Crafted from highly figured Thuya burl, prized for its intricate grain and rich, warm tones. A custom-poured epoxy river, available in deep sapphire blue or translucent black, flows through the natural live edge of the wood. The expansive, smooth surface provides an inspiring and functional area for work or creativity. Each desk is meticulously handcrafted and sits atop modern, sturdy steel legs. Finished with a durable, satin Osmo Polyx-Oil to protect the wood and provide a natural feel.",
+    "originalPrice": 11000.00,
+    "price": 9000.00,
+    "category": "Desks",
+    "size": "150cm x 80cm x 75cm",
+    "featured": true,
+    "images": [
+      "/pd311.jpeg"
+    ]
+  },
+  {
+    "id": 33,
+    "name": "Walnut Gaming Desk",
+    "description": "Elevate your gaming setup with this stunning Walnut & Epoxy River Desk. Crafted from highly figured Walnut, prized for its intricate grain and rich, warm tones. A custom-poured epoxy river, available in deep sapphire blue or translucent black, flows through the natural live edge of the wood. The expansive, smooth surface provides an inspiring and functional area for work or gaming. Each desk is meticulously handcrafted and sits atop modern, sturdy steel legs. Finished with a durable, satin Osmo Polyx-Oil to protect the wood and provide a natural feel.",
+    "originalPrice": 9000.00,
+    "price": 7000.00,
+    "category": "Desks",
+    "size": "125cm x 80cm x 75cm",
+    "featured": true,
+    "images": [
+      "/pd331.jpeg",
+    ]
+  },
+  {
+    "id": 32,
+    "name": "Walnut & Epoxy River Desk",
+    "description": "Elevate your workspace with this stunning Walnut & Epoxy River Desk. Crafted from highly figured Walnut, prized for its intricate grain and rich, warm tones. A custom-poured epoxy river, available in deep sapphire blue or translucent black, flows through the natural live edge of the wood. The expansive, smooth surface provides an inspiring and functional area for work or creativity. Each desk is meticulously handcrafted and sits atop modern, sturdy steel legs. Finished with a durable, satin Osmo Polyx-Oil to protect the wood and provide a natural feel.",
+    "originalPrice": 11000.00,
+    "price": 9000.00,
+    "category": "Desks",
+    "size": "140cm x 70cm x 75cm",
+    "featured": true,
+    "images": [
+      "/pd311.jpeg"
     ]
   },
 ];
